da69b9bca2ab123cb1bc2ecc5f48ea9e736e84c0
Zihan Wang <philwang612@gmail.com>
2024-01-18 01:50:47 +0200

    remedy

--- e02/E02.java
+++ e02/E02.java
@@ -3,10 +3,10 @@ class E02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give a number:");
+        System.out.println("Give a number: ");
         int number1 = scanner.nextInt();
 
-        System.out.println("Give another number:");
+        System.out.println("Give another number: ");
         int number2 = scanner.nextInt();
 
         int sum = number1 + number2;
@@ -15,8 +15,8 @@ class E02 {
         int quotient = number1 / number2;
 
         System.out.println(number1 + " + " + number2 + " = " + sum);
-        System.out.println(number1 + " * " + number2 + " = " + product);
         System.out.println(number1 + " - " + number2 + " = " + difference);
+        System.out.println(number1 + " x " + number2 + " = " + product);
         System.out.println(number1 + " / " + number2 + " = " + quotient);
     }
 }
