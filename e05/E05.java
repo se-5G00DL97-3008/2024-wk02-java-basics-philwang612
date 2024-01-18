5d2909c228d7cf67b408d51b8682462032cec3cb
Zihan Wang <philwang612@gmail.com>
2024-01-18 02:02:27 +0200

    remedy

--- e05/E05.java
+++ e05/E05.java
@@ -3,7 +3,7 @@ class E05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number:");
         int number = scanner.nextInt();
         switch (number) {
             case 1:
